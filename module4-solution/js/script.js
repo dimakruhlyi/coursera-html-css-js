bf7be6664e15a29e9da026ccd440605da229d6ea
dimakruhlyi <dimakruhly@gmail.com>
2020-04-11 14:10:12 +0300

    fixed solution according to instruction

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -1,19 +1,11 @@
-let names = ["Dima", "John", "Jen", "Jason", "Paul", "Frank", "Jim", "Larry", "Paula", "Laura"];
-names[50] = "Nikita"; 
-names[100] = 'Joffre';
-
-function sayHello(name){
-    console.log("Hello, "+ name + "!");
-}
-function sayGoodbye(name){
-    console.log("Goodbye, " + name + "!");
-}
-
-names.forEach(function(item){
-    let firstLetter = item.charAt(0).toLowerCase();
-        if (firstLetter === 'j') {
-          sayGoodbye(item);
+(function() { 
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var name of names) {
+        var firstLetter = name.charAt(0).toLowerCase();
+        if (firstLetter == "j") {
+            byeSpeaker.speak(name);
         } else {
-          sayHello(item);
-        }
-});
+            helloSpeaker.speak(name);
+        } 
+    }
+})();
